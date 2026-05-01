b037246b51f3c13f175ea738af53e379ed457d0b
jeniffertomazoli-hub <jeniffer.tomazoli@dna360.ag>
2026-04-30 22:35:29 -0300

    fix files

--- src/supabase.js
+++ src/supabase.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_SUPABASE_URL
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-if (!url || !key) {
-  console.error('⚠️ Variáveis VITE_SUPABASE_URL e VITE_SUPABASE_ANON_KEY não configuradas.')
-}
-
-export const sb = createClient(url, key)
+export const sb = createClient(
+  'https://ngpucqvhtrgywpgrthdv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ncHVjcXZodHJneXdwZ3J0aGR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczOTEwNzcsImV4cCI6MjA5Mjk2NzA3N30.f4__EwE2gnyR0TBoxKihIYGvKj4ETdPTuDv8s-VEoJI'
+)
